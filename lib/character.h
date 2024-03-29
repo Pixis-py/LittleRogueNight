b96f33b02aeb552fb9945c2d656fd3fb4a1de4c0
Maelig Pesantez <79411634+Pixis-py@users.noreply.github.com>
2024-03-29 14:40:14 +0100

    Update character.h

--- lib/character.h
+++ lib/character.h
@@ -18,6 +18,7 @@ typedef struct character_s character_t;
 /// @brief Structure charcter_t is used to stock the character's infomrations
 typedef struct character_s{
    int pv;
+   int damage;
 }character_t;
 
 /// @brief Prototype of function create which is used to create a character by giving a pointer enough memory to point a structure
@@ -26,7 +27,12 @@ void create(character_t ** c);
 
 /// @brief Prototype of function destruct which is used to destruct a character by freeing it's pointer and make it point on NULL
 /// @param c 
-void destruct(character_t * c);
+void destruct(character_t ** c);
+
+/// @brief Prototype of function attack which is used by "character_t * c" to attack the ennemy (character_t * e).
+/// @param c 
+/// @param e 
+void attack(character_t * c, character_t * e);
 
 /// @brief Prototype of function pv_loss which is used to make the character lose a certain amount of PV given by int a
 /// @param c 
