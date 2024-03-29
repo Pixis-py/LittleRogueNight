3795649b6761ab17dcb6856a0d300a1956778c8d
Maelig Pesantez <79411634+Pixis-py@users.noreply.github.com>
2024-03-29 16:24:26 +0100

    Update image.c

--- src/image.c
+++ src/image.c
@@ -1,5 +1,20 @@
+/**
+ * @file image.c
+ * @author Maelig Pesantez
+ * @brief image.c is used to simplify the creation of an SDL texture so it's not obligated to rewrite 15+lines to create one
+ * @version 0.1
+ * @date 2024-03-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include "../lib/image.h"
 
+/// @brief Function loadTexture is used to create a texture and load it so we don't have to rewrite 15+ lines of code when we have to create one
+/// @param imagePath 
+/// @param renderer 
+/// @return texture
 SDL_Texture* loadTexture(const char* imagePath, SDL_Renderer* renderer) {
     SDL_Surface* surface = IMG_Load(imagePath);
     if (!surface) {
