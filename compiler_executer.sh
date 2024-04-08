e10dd2a33cd25e4e19a0b9124561f6d1b9d0a580
Clay19 <117201202+clay-l1@users.noreply.github.com>
2024-04-08 09:49:54 +0200

    Add files via upload

---
+++ compiler_executer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Faire un 'make all' des fichiers (supposant que vous avez un Makefile configuré)
+make all
+
+# Se rendre dans le dossier 'bin'
+cd bin
+
+# Exécuter le programme 'LittleRogueLike'
+./LittleRogueNight
