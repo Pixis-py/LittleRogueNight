cbd9953a69c4706de081ebd4a0e8a24a7c17e96a
Maelig Pesantez <79411634+Pixis-py@users.noreply.github.com>
2024-04-08 10:06:15 +0200

    Update bouton.c
    Adding doxygen comments

--- src/bouton.c
+++ src/bouton.c
@@ -1,9 +1,27 @@
+/**
+ * @file bouton.c
+ * @author Enzo Desfaudais
+ * @brief bouton.c is used to create buttons more easily
+ * @version 0.1
+ * @date 2024-04-08
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include <SDL2/SDL.h>
 #include "../lib/bouton.h"
 
 
+/// @brief aff_bout creates an SDL_RenderCopy of a button with a normal vue and a hoover vue
+/// @param renderer 
+/// @param bout 
+/// @param e 
+/// @param img 
+/// @param img2 
+/// @return 1 or 0
 extern
-int aff_bout(SDL_Renderer *renderer,SDL_Rect *bout, SDL_Event e,SDL_Texture * img,SDL_Texture * img2){
+int aff_bout(SDL_Renderer *renderer, SDL_Rect *bout, SDL_Event e, SDL_Texture * img, SDL_Texture * img2){
     if (e.motion.x > bout->x && e.motion.x < bout->x  + bout->w && e.motion.y > bout->y && e.motion.y < bout->y + bout->h) {
         SDL_RenderCopy(renderer,img,NULL,bout);
         if(e.type == SDL_MOUSEBUTTONDOWN){
