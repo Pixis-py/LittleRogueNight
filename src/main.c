466986824fe457692607e47ace0b3a02cb19c6e3
Clay19 <117201202+clay-l1@users.noreply.github.com>
2024-04-17 15:09:53 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,11 +9,12 @@
 
 int main(){
     int choice = 0;
+    srand(time(NULL));
     int * choix_perso = malloc(sizeof(int)); /*Permet à l'utilisateur de choisir son personnage dans le menu*/
     *choix_perso = 0;
     printf("Program launch...\n");
     choice = menu(0, NULL,choix_perso); // Lancement du menu
-    while(choice !=0){
+    while(choice !=2){
         if(choice == 1){
             int lab[N][M];
             creer_lab(lab);
@@ -21,15 +22,33 @@ int main(){
                 if(*choix_perso == 0){/*Cas ou l'utilisateur choisi de jouer avec six*/
                     character_t * six; // Déclaration du personnage
                     create(&six,NB_PV_JOUEUR); // Création du personnage
-                    choice = anim(0, NULL, lab, 1, &six); // Boucle de jeu (labyrinthe et animation)
+                    choice = anim(0, NULL, lab, 1, &six);
+                    if(choice == 1){
+                        creer_lab(lab);
+                        if(chercher_chemin(lab, N - 1, M - 1, 0, 0) == 1){
+                            choice = anim(0, NULL, lab, 2, &six);
+                            if(choice == 1){
+                                creer_lab(lab);
+                                if(chercher_chemin(lab, N - 1, M - 1, 0, 0) == 1)
+                                    choice = anim(0, NULL, lab, 3, &six);
+                            }
+                        }
+                    }
                     destruct(six); // Destruction du personnage
                 }
-                if(*choix_perso == 1){ /*Cas ou l'utilisateur choisi de jouer avec mono*/
-                    character_t * mono; // Déclaration du personnage
-                    create(&mono,NB_PV_JOUEUR); // Création du personnage
-                    choice = anim(0, NULL, lab, 1, &mono); // Boucle de jeu (labyrinthe et animation)
-                    destruct(mono); // Destruction du personnage
+            }
+            if(*choix_perso == 1){ /*Cas ou l'utilisateur choisi de jouer avec mono*/
+                character_t * mono; // Déclaration du personnage
+                create(&mono,NB_PV_JOUEUR); // Création du personnage
+                choice = anim(0, NULL, lab, 1, &mono);
+                creer_lab(lab);
+                if(chercher_chemin(lab, N - 1, M - 1, 0, 0) == 1){
+                    choice = anim(0, NULL, lab, 2, &mono);
+                    creer_lab(lab);
+                    if(chercher_chemin(lab, N - 1, M - 1, 0, 0) == 1)
+                        choice = anim(0, NULL, lab, 3, &mono);
                 }
+                destruct(mono); // Destruction du personnage
             }
         }
     }
