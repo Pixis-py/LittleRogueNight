4732580a85be8cc5607ce4058dfc844b10797afb
Maelig Pesantez <79411634+Pixis-py@users.noreply.github.com>
2024-04-17 13:57:50 +0200

    Update anim.h

--- lib/anim.h
+++ lib/anim.h
@@ -2,10 +2,13 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
-#include "../lib/labyrinthe.h"
+#include "image.h"
+#include "character.h"
 
 #define FONT_PATH "arial.ttf"
 
-int anim(int argc, char** argv, int lab[N][M], int niveau);
+void creer_coord(character_t ** ent, int lab[N][M]);
+
+int anim(int argc, char** argv, int lab[N][M], int niveau, character_t ** player);
 
 int message_joueur(const char * text, int x, int y, SDL_Window * pWindow);
