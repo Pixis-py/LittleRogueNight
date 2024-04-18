59d62dd016e53330e1eb0ea01f56648544ef80e9
Maelig Pesantez <79411634+Pixis-py@users.noreply.github.com>
2024-04-18 15:55:56 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main(){
             if(chercher_chemin(lab, N - 1, M - 1, 0, 0)){ 
                 if(*choix_perso == 0){/*Cas ou l'utilisateur choisi de jouer avec six*/
                     character_t * six; // Déclaration du personnage
-                    create(&six,NB_PV_JOUEUR); // Création du personnage
+                    create(&six,NB_PV_JOUEUR, 50); // Création du personnage
                     choice = anim(0, NULL, lab, 1, &six);
                     if(choice == 1){
                         creer_lab(lab);
@@ -42,7 +42,7 @@ int main(){
             }
             if(*choix_perso == 1){ /*Cas ou l'utilisateur choisi de jouer avec mono*/
                 character_t * mono; // Déclaration du personnage
-                create(&mono,NB_PV_JOUEUR); // Création du personnage
+                create(&mono,NB_PV_JOUEUR, 50); // Création du personnage
                 choice = anim(0, NULL, lab, 1, &mono);
                 creer_lab(lab);
                 if(chercher_chemin(lab, N - 1, M - 1, 0, 0) == 1){
