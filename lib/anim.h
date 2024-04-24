ede88ef5e7397b0c85ff2bd82a7b7bee94a2a84e
Clay19 <117201202+clay-l1@users.noreply.github.com>
2024-04-24 14:00:46 +0200

    Update anim.h

--- lib/anim.h
+++ lib/anim.h
@@ -1,3 +1,14 @@
+/**
+ * @file anim.h
+ * @author Clément Lelandais Maelig Pesantez Enzo Desfaudais
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
@@ -5,10 +16,23 @@
 #include "image.h"
 #include "character.h"
 
-#define FONT_PATH "arial.ttf"
-
+/**
+ * @brief Prototype of function creer_coord which generate valid coordonates randomly and assign them to the entity 
+ * 
+ * @param ent 
+ * @param lab 
+ */
 void creer_coord(character_t ** ent, int lab[N][M]);
 
+/**
+ * @brief Main function which uses to manage Six's animations, therefore movements, collisions and also entity management from coordonates given by the creer_coord function
+ * 
+ * @param argc 
+ * @param argv 
+ * @param lab 
+ * @param niveau 
+ * @param player 
+ * @return int 
+ */
 int anim(int argc, char** argv, int lab[N][M], int niveau, character_t ** player);
 
-int message_joueur(const char * text, int x, int y, SDL_Window * pWindow);
